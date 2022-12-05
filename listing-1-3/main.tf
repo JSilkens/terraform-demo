0b653cc99bfac88ddb75c864f7578e85f210c164
Johan Silkens <me@jsilkens.be>
2022-12-05 14:26:12 +0100

    Add listing 1-3

--- listing-1-3/main.tf
+++ listing-1-3/main.tf
@@ -2,6 +2,24 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "name" {
-  
+data "aws_ami" "ubuntu" { # Declares an aws_ami data source with the name "ubuntu"
+  most_recent = true
+
+  filter { #Sets a filter to select all AMI's with the name mathing this regex expression
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+
+  }
+
+  owners = ["099720109477"] # Canonical Ubuntu AWS account id 
 }
+
+
+resource "aws_instance" "helloworld" {
+  ami           = data.aws_ami.ubuntu.id # Chains resources together
+  instance_type = "t2.micro"
+  tags = {
+    name = "HelloWorld"
+  }
+}
+
