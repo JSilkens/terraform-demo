9bbcff9497312390237b81510521a35ff94dd7d8
Johan Silkens <me@jsilkens.be>
2022-12-05 15:15:21 +0100

    Added listing 2-1

---
+++ listing-2-1/main.tf
@@ -0,0 +1,23 @@
+terraform { #Terraform setting block
+  required_version = ">= 0.15"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "local_file" "literature" {
+  filename = "art_of_war.txt"
+  content  = <<-EOT
+   Sun Tzu said: The art of war is of vital importance to the State.
+   It is a matter of life and death, a road either to safety or to ruin.
+   Hence it is a subject of inquiry which can on no account be neglected. 
+   EOT 
+  #Heredoc syntax for multiline strings
+  #   Tip The <<- sequence indicates an indented heredoc string.
+  #    Anything between the opening identifier and the closing identifier (EOT) is interpreted literally.
+  #     Leading whitespace, however, is ignored (unlike traditional heredoc syntax).
+}
