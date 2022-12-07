7d92abe4dba875fb607249a71dbddd65b79cc037
Johan Silkens <johan@synapse.org>
2022-12-07 15:07:14 +0100

    Completed chapter 3

--- listing-3-7/madlibs.tf
+++ listing-3-7/madlibs.tf
@@ -16,30 +16,41 @@ variable "words" {
     verbs      = list(string),
     adverbs    = list(string),
     numbers    = list(string),
-  })        
+  })
 }
 
+variable "num_files" { #Declares an input variable for setting count on the random_shuffele resources
+  default = 100
+  type    = number
+}
+
+
 locals {
-  uppercase_words = {for k, v in var.words : k => [for s in v : upper(s)]} # for expression to uppercase strings and save to a local value
+  uppercase_words = { for k, v in var.words : k => [for s in v : upper(s)] } # for expression to uppercase strings and save to a local value
 }
 
 resource "random_shuffle" "random_nouns" {
+  count = var.num_files                  # References the num_files variable to dynamically set the count meta argument
   input = local.uppercase_words["nouns"] # A new shuffeled list is generated from the input list
 }
 
 resource "random_shuffle" "random_adjectives" {
+  count = var.num_files
   input = local.uppercase_words["adjectives"]
 }
 
 resource "random_shuffle" "random_verbs" {
+  count = var.num_files
   input = local.uppercase_words["verbs"]
 }
 
 resource "random_shuffle" "random_adverbs" {
+  count = var.num_files
   input = local.uppercase_words["adverbs"]
 }
 
 resource "random_shuffle" "random_numbers" {
+  count = var.num_files
   input = local.uppercase_words["numbers"]
 }
 
